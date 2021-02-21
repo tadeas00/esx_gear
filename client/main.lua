58afb68a302e2ee93029820c33353fa0e9337d88
tadeas00 <77239884+tadeas00@users.noreply.github.com>
2021-02-21 21:42:20 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -9,23 +9,7 @@ end)
 --kyslíkovka--
 RegisterNetEvent('esx_gear:kyslikovamaska')
 AddEventHandler('esx_gear:kyslikovamaska', function()
-    local playerPed = GetPlayerPed(-1)
-    local coords = GetEntityCoords(playerPed)
-    local boneIndex = GetPedBoneIndex(playerPed, 12844)
-    local boneIndex2 = GetPedBoneIndex(playerPed, 24818)
-    ESX.Game.SpawnObject('p_s_scuba_mask_s', {
-		x = coords.x,
-		y = coords.y,
-		z = coords.z - 3
-	}, function(object)
-		ESX.Game.SpawnObject('p_s_scuba_tank_s', {
-			x = coords.x,
-			y = coords.y,
-			z = coords.z - 3
-		}, function(object2)
-			AttachEntityToEntity(object2, playerPed, boneIndex2, -0.30, -0.22, 0.0, 0.0, 90.0, 180.0, true, true, false, true, 1, true)
-			AttachEntityToEntity(object, playerPed, boneIndex, 0.0, 0.0, 0.0, 0.0, 90.0, 180.0, true, true, false, true, 1, true)
-			SetPedDiesInWater(playerPed, false)
+    IN DEV
 			
 			ESX.ShowNotification(_U('usedoxygen') .. '%.')
 			Citizen.Wait(100000)
