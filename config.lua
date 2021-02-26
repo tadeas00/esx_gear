5d9f2889c1dbfd6efb2884564ff32588cc95fb77
tadeas00 <77239884+tadeas00@users.noreply.github.com>
2021-02-26 17:34:24 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,2 +1,45 @@
 Config        = {}
 Config.Locale = 'cs'
+
+Config.SkinId = {
+    maleDiving = {
+        glasses_1 = 26, glasses_2 = 0, 
+        tshirt_1 = 179, tshirt_2 = 0
+    },
+
+    femaleDiving = {
+        glasses_1 = 26, glasses_2 = 0, 
+        tshirt_1 = 179, tshirt_2 = 0
+    },
+
+    maleNight = {
+        helmet_1 = 116, helmet_2 = 0
+    },
+
+    femaleNight = {
+        helmet_1 = 115, helmet_2 = 0
+    },
+
+    maleThermal = {
+        helmet_1 = 118, helmet_2 = 0
+    },
+
+    femaleThermal = {
+        helmet_1 = 117, helmet_2 = 0
+    },
+
+    Nightrest = {
+        helmet_1 = -1, helmet_2 = 0
+    },
+
+    thermalrest = {
+        helmet_1 = -1, helmet_2 = 0
+    },
+
+    oxygenrest = {
+        glasses_1 = 0, glasses_2 = 0, 
+        tshirt_1 = 0, tshirt_2 = 0
+    }
+
+
+}
