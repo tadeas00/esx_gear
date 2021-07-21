f9175967b31f05ba1e16e10820cf15b25a9c420f
tadeas00 <77239884+tadeas00@users.noreply.github.com>
2021-07-21 13:25:29 +0200

    Update esx_gear.sql

--- esx_gear.sql
+++ esx_gear.sql
@@ -2,4 +2,5 @@ INSERT INTO `items` (`name`, `label`, `limit`) VALUES
 	('nightvision', 'Night Vision', 1),
     ('kyslikovamaska', 'Kyslíková Maska', 1),
     ('thermalvision', 'Thermal Vision', 1)
+    ('gasmask', 'Plynová maska', 1)
 ;
