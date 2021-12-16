2f0b9cecb15cb8e272be5ea7907f1e190b27bde0
tadeas00 <77239884+tadeas00@users.noreply.github.com>
2021-12-16 21:57:11 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -4,4 +4,6 @@ Locales ['en'] = {
     ['nightvision'] = 'You have used ~b~Night Vision~s~ Battery Capacity: ~g~100~w~',
     ['vybijeni'] = '~g~Battery Capacity:~y~ %s%s~w~',
     ['thermalvision'] = 'You have used ~o~Thermal Vision~s~ Battery capacity:',
+    ['gasmask'] = 'You have used ~o~Gas Mask~y~. Filter endurance: ~g~100~w~',
+    ['filtr'] = '~g~Filter endurance:~y~ %s%s~w~',
 }
